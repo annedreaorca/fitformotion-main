8af98cb1c891a0b8ea54a24c62711c15b8926d1d
AndreaOrca <104371869+999dengA@users.noreply.github.com>
2024-08-31 08:36:59 +0800

    commitment

--- app/(protected)/gallery/page.tsx
+++ app/(protected)/gallery/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import PageHeading from "@/components/PageHeading/PageHeading";
 
-export default async function ActivityPage() {
+export default async function Gallery() {
   return (
     <div className="page-container">
       <PageHeading title="Gallery" />
