c7aeca8f5a481248731a2a219dc49337a392a0a7
cjaycuya <cjapaycuya16@gmail.com>
2024-10-21 01:15:20 +0800

    middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+const isBot = (req: NextRequest) => {
+  const userAgent = req.headers.get("user-agent") || "";
+  return /Googlebot|bingbot|Baiduspider|YandexBot|DuckDuckBot|Slurp/.test(userAgent);
+};
 
 export default authMiddleware({
   publicRoutes: ["/", "/about-us", "/features", "/pricing", "/faq", "/references"],
+  beforeAuth(req) {
+    if (isBot(req)) {
+      return NextResponse.next(); // Allow bots to proceed
+    }
+  },
 });
 
 export const config = {
