828e8f99db4e6459b50105b364fbb5ad427c65b8
cjaycuya <cjapaycuya16@gmail.com>
2024-10-21 00:56:21 +0800

    Removed noindex meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default async function RootLayout({
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
         />
         <link rel="canonical" href="https://fitformotion.com/"/>
-        <meta name="robots" content="noindex"/>
       </head>
       <body
         className={`${alexandria.className} flex flex-col grow overflow-x-hidden`}
