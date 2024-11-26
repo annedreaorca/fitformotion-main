7d784273b89a97378f1a217bafacd98c2492c298
cjaycuya <cjapaycuya16@gmail.com>
2024-11-27 04:23:30 +0800

    Production key  and Secret key changed

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-const isBot = (req: NextRequest) => {
-  const userAgent = req.headers.get("user-agent") || "";
-  return /Googlebot|bingbot|Baiduspider|YandexBot|DuckDuckBot|Slurp/.test(userAgent);
-};
 
 export default authMiddleware({
   publicRoutes: ["/", "/about-us", "/features", "/pricing", "/faq", "/references"],
-  beforeAuth(req) {
-    // Allow bots to proceed without authentication
-    if (isBot(req)) {
-      return NextResponse.next();
-    }
-  },
 });
 
 export const config = {
